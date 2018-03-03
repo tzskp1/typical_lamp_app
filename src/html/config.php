3064bde86b85508bdd99241a6d98a6968f484f48
Tanaka Kazunari <tzskp1@gmail.com>
2018-03-03 14:07:27 +0900

    password setting's are collected in 1 place (i.e. .env).

--- src/html/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-  define('DBNAME', "mysql:host=db;dbname=BBS;charset=utf8mb4");
-  define('DBUSER', "BBS");
-  define('DBPASS', "password");
---
+++ src/include/config.php
@@ -0,0 +1,4 @@
+<?php
+  define('DBNAME', "mysql:host=db;dbname=BBS;charset=utf8mb4");
+  define('DBUSER', "USER_QSzaADdHtL"); //
+  define('DBPASS', "PASS_GRzFZRcwng"); // will be replaced by real password.
